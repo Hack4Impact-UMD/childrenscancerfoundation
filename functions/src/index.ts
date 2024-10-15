384f2985874d89af191c05ae49b117ac322b6cf5
Jaskaran Gill <jgill12@terpmail.umd.edu>
2024-10-15 17:46:47 -0400

    basic email firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,39 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
+import * as sendgridMail from "@sendgrid/mail";
+
+const apiKey = process.env.SENDGRID_API_KEY;
+if (!apiKey) {
+  throw new Error("SendGrid API key is not set in the environment variables.");
+}
+sendgridMail.setApiKey(apiKey);
+
+// email sending function
+export const sendEmail = functions.https.onRequest(async (req, res) => {
+  // Extract email parameters from request body
+  const { to, from, subject, text, html } = req.body;
+
+  // Construct the email message
+  const msg = {
+    to,
+    from,
+    subject,
+    text,  // Plain text content
+    html,  // HTML content (optional)
+  };
+
+  try {
+    // Send the email
+    await sendgridMail.send(msg);
+    console.log("Email sent successfully!");
+    res.status(200).send("Email sent successfully!");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Failed to send email");
+  }
+});
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
