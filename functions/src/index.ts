fc6181c50a3ce07f50ddca70259b177917d963cf
ipab1 <ipabba@terpmail.umd.edu>
2024-11-11 22:53:35 -0500

    Updated retrieve reviewers function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,7 @@
 
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import { getAuth, UserRecord } from "firebase-admin/auth";
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
@@ -20,32 +21,72 @@ admin.initializeApp();
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-export const addReviewerRole = functions.https.onCall((data: any, context: any) => {
-    return admin.auth().getUserByEmail(data.email).then((user: any) => {
+export const addReviewerRole = functions.https.onCall(
+  (data: any, context: any) => {
+    return admin
+      .auth()
+      .getUserByEmail(data.email)
+      .then((user: any) => {
         return admin.auth().setCustomUserClaims(user.uid, {
-            role: "reviewer"
+          role: "reviewer",
         });
-    }
-    ).then(() => {
+      })
+      .then(() => {
         return {
-            message: `Success! ${data.email} has been made a reviewer.`
+          message: `Success! ${data.email} has been made a reviewer.`,
         };
-    }).catch((err: any) => {
+      })
+      .catch((err: any) => {
         return err;
-    });
-});
+      });
+  }
+);
 
-export const addApplicantRole = functions.https.onCall((data: any, context: any) => {
-    return admin.auth().getUserByEmail(data.email).then((user: any) => {
+export const addApplicantRole = functions.https.onCall(
+  (data: any, context: any) => {
+    return admin
+      .auth()
+      .getUserByEmail(data.email)
+      .then((user: any) => {
         return admin.auth().setCustomUserClaims(user.uid, {
-            role: "applicant"
+          role: "applicant",
         });
-    }
-    ).then(() => {
+      })
+      .then(() => {
         return {
-            message: `Success! ${data.email} has been made an applicant.`
+          message: `Success! ${data.email} has been made an applicant.`,
         };
-    }).catch((err: any) => {
+      })
+      .catch((err: any) => {
         return err;
-    });
+      });
+  }
+);
+
+export const getReviewers = onRequest(async (req, res) => {
+  try {
+    const reviewerUserIds: string[] = [];
+
+    const listAllUsers = async (nextPageToken?: string) => {
+      const listUsersResult = await getAuth().listUsers(1000, nextPageToken);
+      listUsersResult.users.forEach((userRecord: UserRecord) => {
+        if (
+          userRecord.customClaims &&
+          userRecord.customClaims["role"] === "reviewer"
+        ) {
+          reviewerUserIds.push(userRecord.uid);
+        }
+      });
+      if (listUsersResult.pageToken) {
+        await listAllUsers(listUsersResult.pageToken);
+      }
+    };
+
+    await listAllUsers();
+
+    res.status(200).json({ reviewers: reviewerUserIds });
+  } catch (error) {
+    logger.error("Error retrieving reviewers:", error);
+    res.status(500).send("Failed to retrieve reviewers");
+  }
 });
