700fb9acaef056d358b9f8236c2b3257ff102011
Tara <you@example.com>
2024-11-12 17:16:17 -0500

    changes

--- react-app/ccf/src/settings/settings.tsx
+++ react-app/ccf/src/settings/settings.tsx
@@ -1,35 +1,55 @@
-import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getFunctions, httpsCallable } from "firebase/functions";
-import {
-  getAuth,
-  updatePassword,
-} from "firebase/auth";
-import { getFirestore, doc, setDoc, deleteDoc } from "firebase/firestore";
+import { auth } from "../index";
 import { checkPasswordRequirements } from "../users/userpasswords";
 import "./Settings.css";
 import logo from '../assets/ccf-logo.png';
 import Sidebar from "../sidebar/Sidebar";
-
+import { getApplicantUser, editApplicantUser } from "../users/usermanager";
+import { changePassword } from "../users/userpasswords";
+import { loginUser } from "../users/auth_login";
 function AccountSettingsPage(): JSX.Element {
-    //form inputs
+
+    // authentication information
+    const [username, setUsername] = useState<string | null>(null);
+    const [currentPassword, setCurrentPassword] = useState("");
     const [pwd, setPwd] = useState("");
     const [confirmPwd, setConfirmPwd] = useState("");
-  
-    //password reqs
+
+    // password requirements
     const [specialChar, setSpecialChar] = useState(false);
     const [capitalLetter, setCapitalLetter] = useState(false);
     const [number, setNumber] = useState(false);
     const [showReqs, setShowReqs] = useState(false);
     const [pwdUnmatched, setPwdUnmatched] = useState(false);
+    const [currentPasswordMatched, setCurrentPasswordMatched] = useState(false);
 
+    // personal information
+    const [firstName, setFirstName] = useState("first name not found");
+    const [lastName, setLastName] = useState("last name not found");
+    const [title, setTitle] = useState("title not found");
+    const [institution, setInstitution] = useState("institution not found");
 
-    const [firstName, setFirstName] = useState("John");
-    const [lastName, setLastName] = useState("Smith");
-    const [title, setTitle] = useState("M.D.");
-    const [institution, setInstitution] = useState("Institution Name");
-
-
+    useEffect(() => {
+        const fetchUserData = async () => {
+            if (auth.currentUser?.uid) {
+                try {
+                    const user = await getApplicantUser(auth.currentUser.uid);
+                    setTitle(user?.title || "title not found");
+                    setFirstName(user?.name.split(" ")[0] || "first name not found");
+                    setLastName(user?.name.split(" ")[1] || "last name not found");
+                    setInstitution(user?.institutionalAffiliation || "institution not found");
+                } catch (error) {
+                    console.error("Error fetching user data:", error);
+                    setTitle("title not found");
+                    setFirstName("first name not found");
+                    setLastName("last name not found");
+                    setInstitution("institution not found");
+                }
+            }
+        };
+        fetchUserData();
+    }, [auth.currentUser]);
+    
     const checkConfirmPwd = () => {
         if (confirmPwd !== "") {
           confirmPwd === pwd ? setPwdUnmatched(false) : setPwdUnmatched(true);
@@ -37,30 +57,15 @@ function AccountSettingsPage(): JSX.Element {
     };
 
     const handleSubmit = async (e: any) => {
-        // don't let user submit if pwd reqs aren't met
+        // don't let user submit if pwd reqs aren't met or current password not confirmed
         e.preventDefault();
-        const functions = getFunctions();
-        console.log(specialChar, capitalLetter, number, showReqs, pwdUnmatched);
-        if (!specialChar || !capitalLetter || !number || pwdUnmatched) {
+        console.log(specialChar, capitalLetter, number, showReqs, pwdUnmatched, currentPasswordMatched);
+        if (!specialChar || !capitalLetter || !number || pwdUnmatched || !currentPasswordMatched) {
           console.log("Failed to submit. One requirement was not met.");
           e.preventDefault();
           return;
         }
-
-        const auth = getAuth();
-        const db = getFirestore();
-        const user = auth.currentUser;
-
-        if (user) {
-            try {
-                await updatePassword(user, pwd);
-                console.log("Password updated successfully.");
-            } catch (error) {
-                console.error("Error updating password:", error);
-            }
-        } else {
-            console.log("No user is logged in.");
-        }
+        changePassword(pwd);
 
     };
     
@@ -86,45 +91,178 @@ function AccountSettingsPage(): JSX.Element {
                         </div>
                     
                         <div className="AccountSetting-personal-info">
-                            <div className="info-row">
+                            <div className="AccountSetting-personal-info-field">
                                 <label>First Name</label>
-                                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                                <span className="edit-icon">✎</span>
+                                <div className="info-row">
+                                    <input type="text" value={firstName} onChange={(e) => editApplicantUser(auth.currentUser?.uid || "", {name: e.target.value + " " + lastName})} />
+                                    <span className="edit-icon">✎</span>
+                                </div>
                             </div>
-                            <div className="info-row">
+                            <div className="AccountSetting-personal-info-field">
                                 <label>Last Name</label>
-                                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                                <span className="edit-icon">✎</span>
+                                <div className="info-row">
+                                    <input type="text" value={lastName} onChange={(e) => editApplicantUser(auth.currentUser?.uid || "", {name: firstName + " " + e.target.value})} />
+                                    <span className="edit-icon">✎</span>
+                                </div>
                             </div>
-                            <div className="info-row">
+                            <div className="AccountSetting-personal-info-field">
                                 <label>Title</label>
-                                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-                                <span className="edit-icon">✎</span>
+                                <div className="info-row">
+                                    <input type="text" value={title} onChange={(e) => editApplicantUser(auth.currentUser?.uid || "", {title: e.target.value})} />
+                                    <span className="edit-icon">✎</span>
+                                </div>
                             </div>
-                            <div className="info-row">
+                            <div className="AccountSetting-personal-info-field">
                                 <label>Institution</label>
-                                <input type="text" value={institution} onChange={(e) => setInstitution(e.target.value)} />
-                                <span className="edit-icon">✎</span>
+                                <div className="info-row">
+                                    <input type="text" value={institution} onChange={(e) => editApplicantUser(auth.currentUser?.uid || "", {institutionalAffiliation: e.target.value})} />
+                                    <span className="edit-icon">✎</span>
+                                </div>
                             </div>
                         </div>      
                     </div>
-
-
 
                     <div className="AccountSettings-section">
                         <div className="header-title">
                              <h2>Account Settings</h2>
                          </div>
-                        <div className="info-row">
+                        <div className="info-row-settings">
                             <label>Username</label>
+                            <span>{username ? username : "No username available"}</span>
                         </div>
-                        <div className="info-row">
-                            <label>Password</label>
+                        <div className="info-row-settings">
+                            <label>Current Password</label>
+                            <input
+                                type="current-password"
+                                placeholder="Enter current password"
+                                required
+                                value={currentPassword}
+                                onChange={async (e) => {
+                                    setCurrentPassword(e.target.value);
+                                    const { error: loginError } = await loginUser(auth.currentUser?.email || "", currentPassword);
+                                    if (!loginError) setCurrentPasswordMatched(true);
+                                }}
+                                onFocus={() => setShowReqs(true)} // Show on focus
+                                onBlur={() => setShowReqs(false)}
+                                onKeyUp={checkConfirmPwd}
+                                className="input"
+                            />
+                            {!currentPasswordMatched && (
+                                <p className="validation">Current password is incorrect</p>
+                            )}
                         </div>
+                        <div className="info-row-settings">
+                            <label>New Password</label>
+                            <div className="info-row">
+                                <input
+                                    type="password"
+                                    placeholder="Password1!"
+                                    required
+                                    value={pwd}
+                                    onChange={(e) => {
+                                    setPwd(e.target.value);
+                                    const newRequirements = checkPasswordRequirements(e.target.value);
+                                    setSpecialChar(newRequirements.specialChar);
+                                    setCapitalLetter(newRequirements.capitalLetter);
+                                    setNumber(newRequirements.number);
+                                    }}
+                                    onFocus={() => setShowReqs(true)} // Show on focus
+                                    onBlur={() => setShowReqs(false)}
+                                    onKeyUp={checkConfirmPwd}
+                                    className="input"
+                                />
+                            </div>
+                            {showReqs && (
+                                <div className="pwd-reqs">
+                                <p>Password requires:</p>
+                                <label id="checkbox">
+                                    <input
+                                    type="checkbox"
+                                    name="options"
+                                    value="Yes"
+                                    checked={specialChar}
+                                    readOnly
+                                    />
+                                    One special character
+                                </label>
+                                <label id="checkbox">
+                                    <input
+                                    type="checkbox"
+                                    name="options"
+                                    value="Yes"
+                                    checked={capitalLetter}
+                                    readOnly
+                                    />
+                                    One capital letter
+                                </label>
+                                <label id="checkbox">
+                                    <input
+                                    type="checkbox"
+                                    name="options"
+                                    value="Yes"
+                                    checked={number}
+                                    readOnly
+                                    />
+                                    One number
+                                </label>
+                                </div>
+                            )}
 
-    
+                            {((!specialChar || !number || !capitalLetter) && pwd && !showReqs) &&  (
+                                <p className="validation">At least one password requirement was not met</p>
+                            )}
+                            
+                            <label>Confirm Password</label>
+                            <div
+                                className={
+                                !pwdUnmatched
+                                    ? "confirm-pwd-container"
+                                    : "confirm-pwd-container-exclaim"
+                                }
+                            >
+                                <input
+                                type="password"
+                                placeholder="Confirm New Password"
+                                required
+                                value={confirmPwd}
+                                onChange={(e) => setConfirmPwd(e.target.value)}
+                                onKeyUp={checkConfirmPwd}
+                                className="input"
+                                />
+                                {pwdUnmatched && <p id="exclaim">!</p>}
+                            </div>
+
+                            {pwdUnmatched && (
+                                <p className="validation">Passwords do not match</p>
+                            )}
+                        </div>
+                        <button
+                            type="submit"
+                            className={
+                            !pwd ||
+                            (pwd && !confirmPwd) ||
+                            !specialChar ||
+                            !capitalLetter ||
+                            !number ||
+                            pwdUnmatched
+                                ? "disable-submit"
+                                : "signup-btn2"
+                            }
+                            onClick={handleSubmit}
+                            disabled={
+                            !firstName ||
+                            !lastName ||
+                            !pwd ||
+                            (pwd && !confirmPwd) ||
+                            !specialChar ||
+                            !capitalLetter ||
+                            !number ||
+                            pwdUnmatched
+                            }
+                        >
+                            Change Password
+                        </button>
                     </div>
-
                 </div>
             </div>
         </div>
--- react-app/ccf/src/users/userpasswords.ts
+++ react-app/ccf/src/users/userpasswords.ts
@@ -1,3 +1,7 @@
+import {
+  updatePassword,
+} from "firebase/auth";
+import { auth } from "../index";
 
 // Type definition for password requirements
 export interface PasswordRequirements {
@@ -6,12 +10,25 @@ export interface PasswordRequirements {
     number: boolean;
   }
   
-  // Function to check password requirements
-  export const checkPasswordRequirements = (password: string): PasswordRequirements => {
-    return {
-      specialChar: /[\W_]/.test(password),
-      capitalLetter: /[A-Z]/.test(password),
-      number: /[0-9]/.test(password)
-    };
+// Function to check password requirements
+export const checkPasswordRequirements = (password: string): PasswordRequirements => {
+  return {
+    specialChar: /[\W_]/.test(password),
+    capitalLetter: /[A-Z]/.test(password),
+    number: /[0-9]/.test(password)
+  };
+};
+
+// Change password function
+export const changePassword = async (newPassword: string) => {
+    try {
+      const user = auth.currentUser;
+      if (user) {
+        await updatePassword(user, newPassword);
+      }
+      console.log("Password changed successfully.");
+    } catch (error) {
+      console.error("Error changing password:", error);
+    }
   };
   
