9af6d414db94780d855904b1202e561002a8ac01
avishig <avishigupta1115@gmail.com>
2024-11-13 00:44:20 -0500

    Completed Reviewer Dashboard

--- react-app/ccf/src/App.tsx
+++ react-app/ccf/src/App.tsx
@@ -1,8 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FAQPage from './faq_page/FAQPage';
 import ApplicantUsersDashboard from './applicant-dashboard/ApplicantDashboard';
-import ContactUs from './contact_us_page/ContactPage';
+import ReviewerDashboard from './reviewer-dashboard/ReviewerDashboard';
 
 interface FAQItem {
   question: string;
@@ -66,24 +65,9 @@ function App(): JSX.Element {
             <ApplicantUsersDashboard />
           }
         />
-        {/* Admin dashboard */}
-        <Route
-          path="/admin"
-          element={
-            <></>
-          }
-        />
-        <Route path="/faq" element={<FAQPage faqData={faqData} />} />
-        <Route
-          path="/contact"
-          element={
-            <ContactUs 
-                email="email@email.com" 
-                phone="000-000-0000" 
-                hours="Mon - Fri, 9:00AM - 4PM EST" 
-              />
-          }
-          />
+        {/* Reviewer dashboard */}
+        <Route path="/reviewer-dashboard"  
+          element={<ReviewerDashboard faqData={faqData} email="email@email.com" phone="000-000-0000" hours="Mon - Fri, 9:00AM - 4PM EST" />} />
       </Routes>
     </BrowserRouter>
   );
