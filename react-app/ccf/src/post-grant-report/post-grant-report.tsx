c642caa1d82d1ebaad3746f83eb8690696442bee
jfathi <javidafathi@gmail.com>
2024-12-01 14:07:37 -0500

    WIP - check in

--- react-app/ccf/src/post-grant-report/post-grant-report.tsx
+++ react-app/ccf/src/post-grant-report/post-grant-report.tsx
@@ -20,7 +20,7 @@ function PostGrantReport(): JSX.Element {
 
     return (
         <div className="PostGrantReport">
-            
+
             <div className="PostGrantReport-header-container">
                 <h1 className="PostGrantReport-header">
                     Post Grant Results
@@ -28,72 +28,56 @@ function PostGrantReport(): JSX.Element {
             </div>
 
             <div className="PostGrantReport-sections-content">
+                <div className="PostGrantReport-section-box">
+                    <div className="PostGrantReport-section">
+                        <div className="PostGrantReport-section-header">
+                            <div className="header-title">
+                                <h2>Post-Grant Report</h2>
+                            </div>
+                        </div>
+                        <div className="PostGrantReport-subsection">
+                            <div className="header-title">
+                                <h3>In the Post-Grant Report, please submit a 2-3 page Word or PDF file which includes:</h3>
+                            </div>
+                            <ol>
+                                <li>Research Title</li>
+                                <li>Principal Investigator</li>
+                                <li>Institution</li>
+                                <li>Grant Start and End Dates</li>
+                                <li>Initial Research Goal</li>
+                                <li>Results/Findings - Including relevant graphs, charts, or images</li>
+                                <li>Ongoing/Additional Plans - Such as intent for future research using said findings and intent to submit abstracts on funded research to any research publications (crediting funding from CCF)</li>
+                            </ol>
+                        </div>
+                        <div className="PostGrantReport-subsection">
+                            <div className="header-title">
+                                <h3>Upload File (PDF Format)</h3>
+                            </div>
+                            {/* Click to Upload Button with X option */}
+                        </div>
 
-                <div className="PostGrantReport-section">
-                    <div className="PostGrantReport-section-header">
-                        <div className="header-title">
-                            <h2>Post-Grant Report</h2>
+                        <div className="PostGrantReport-subsection">
+                            <div className="attestation">
+                                <div className="attestation-label">Awardee/Principal Investigator:</div>
+                                {/* Free text input (Legal Name) */}
+                            </div>
+                            <div className="attestation">
+                                <div className="attestation-label">Institution:</div>
+                                {/* Free text input (Institution Name) */}
+                            </div>
+                            <div className="attestation">
+                                <div className="attestation-label">Date:</div>
+                                {/* Date Input (YYYY-MM-DD) */}
+                                {/* <Calendar onChange={onDateChange} value={attestationDate} /> */}
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="PostGrantReport-section">
-                    <div className="PostGrantReport-section-header">
-                        <div className="header-title">
-                            <h3>In the Post-Grant Report, please submit a 2-3 page Word or PDF file which includes:</h3>
-                        </div>
-                    </div>
-                    <ol>
-                        <li>Research Title</li>
-                        <li>Principal Investigator</li>
-                        <li>Institution</li>
-                        <li>Grant Start and End Dates</li>
-                        <li>Initial Research Goal</li>
-                        <li>Results/Findings - Including relevant graphs, charts, or images</li>
-                        <li>Ongoing/Additional Plans - Such as intent for future research using said findings and intent to submit abstracts on funded research to any research publications (crediting funding from CCF)</li>
-                    </ol>
-                </div>
-
-                <div className="PostGrantReport-section">
-                    <div className="PostGrantReport-section-header">
-                        <div className="header-title">
-                            <h3>Upload File (PDF Format)</h3>
-                        </div>
-                    </div>
-                    {/* Click to Upload Button with X option */}
-                </div>
-
-                <div className="PostGrantReport-section">
-                    <div className="PostGrantReport-section-header">
-                        <div className="header-title">
-                            <h3>Upload File (PDF Format)</h3>
-                        </div>
-                    </div>
-                    {/* Click to Upload Button with X option */}
-                </div>
-
-
-                <div className="PostGrantReport-section">
-                    <div className="attestation">
-                        <h5 className="attestation-label">Awardee/Principal Investigator:</h5>
-                        {/* Free text input (Legal Name) */}
-                    </div>
-                    <div className="attestation">
-                        <h5 className="attestation-label">Institution:</h5>
-                        {/* Free text input (Institution Name) */}
-                    </div>
-                    <div className="attestation">
-                        <h5 className="attestation-label">Date:</h5>
-                        {/* Date Input (YYYY-MM-DD) */}
-                        {/* <Calendar onChange={onDateChange} value={attestationDate} /> */}
-                    </div>
+                <div className="submit">
+                    {/* button to save and submit */}
                 </div>
             </div>
-
-            <div className="submit">
-                {/* button to save and submit */}
-            </div>
-
         </div>
     )
 };
